7f2beb9079225bcdeb36b57ea9dea70abd6fae0f
toryzhou <522617505@qq.com>
2019-03-25 14:12:08 +0800

    Add liquibase maven plugin for generating change log

---
+++ src/main/java/com/example/springsecurityrbac/service/LiquibaseService.java
@@ -0,0 +1,12 @@
+package com.example.springsecurityrbac.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : toryzhou
+ * @date : 3/25/19 13:42
+ */
+@Service
+public class LiquibaseService {
+
+}
